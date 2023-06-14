b086308ea6e3d1fcd8403f071dbe570ae0726204
dowoo303 <dowoo303@naver.com>
2023-06-14 16:23:50 +0900

    first commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,20 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const LOCK = await hre.ethers.getContractFactory("Lock"); // 컨트랙트 가져오기
+  const lock = await LOCK.deploy(); // 디플로이
+  console.log(lock.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
